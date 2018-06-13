c9fcf36947c7c57a157facbd432cf11cd20423f0
Tommy Rogers <tommyr@TommyRogers.local>
2018-06-13 15:29:27 -0400

    finished lab

--- IntegerDuplicateDeleter.java
+++ IntegerDuplicateDeleter.java
@@ -1,6 +1,50 @@
- 
-
 
 public class IntegerDuplicateDeleter extends DuplicateDeleter<Integer>{
+    public IntegerDuplicateDeleter(Integer [] array){
+        super(array);
+    }
+
+    public int counterArray(int num) {
+        int count = 0;
+        for(int x: array){
+            if(x == num){
+                count++;
+            }
+        }
+        return count;
+    } 
+
+    
+
+    public Integer[] removeDuplicates(int num){ 
+        Integer[] newArray = new Integer[array.length];
+        int y = 0;
+        for (int x : array) {
+            int occurences = counterArray(x);
+            if (occurences < num) {
+                newArray[y] = x;
+                y++;
+            }
+        }
+        Integer[] finalArray = new Integer[y];
+        System.arraycopy(newArray, 0, finalArray, 0, y);
+        return finalArray;
+    }
+    
+    public Integer [] removeDuplicatesExactly(int num){
+        Integer[] newArray = new Integer[array.length];
+        int y = 0;
+        for (int j: array) {
+            int occurences = counterArray(j);
+            if (occurences != num){
+                newArray[y] = j;
+                y++;
+            }
+        }
+        Integer [] finalArray = new Integer[y];
+        System.arraycopy(newArray, 0, finalArray, 0, y);
+        return finalArray;
+    }
 
 }
+
--- StringDuplicateDeleter.java
+++ StringDuplicateDeleter.java
@@ -1,6 +1,51 @@
- 
 
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String [] array){
+        super(array);
+    }
 
+
+    public int counterArray(String thisElement) {
+        int count = 0;
+        for (String x : array)
+            if (x == thisElement) {
+                count++;
+            }
+        return count;
+    }
+
+        
+    public String[] removeDuplicates(int num){ 
+        String [] newArray = new String [array.length];
+        int y = 0;
+        for (String j : array){
+            int occurences = counterArray(j);
+            if (occurences < num){
+                newArray[y] = j;
+                y++;
+            } 
+        }
+        String [] finalArray = new String [y];
+        System.arraycopy(newArray, 0, finalArray, 0, y);
+        return finalArray;  
+    }
+
+
+    public String [] removeDuplicatesExactly(int num){
+        String [] newArray = new String [array.length];
+        int y = 0;
+            for (String x : array){
+            int occurences = counterArray(x);    
+               if (occurences != num){
+                newArray[y] = x;
+                y++;
+            } 
+        }
+        String [] finalArray = new String [y];
+        System.arraycopy(newArray, 0, finalArray, 0, y);
+        return finalArray; 
+    }
+        
 
 }
+
